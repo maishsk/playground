96d97c814632c55f38b8a4fefd8f54ed1e86a88c
Maish Saidel-Keesing <maishsk@gmail.com>
2020-03-04 23:12:28 +0200

    updated for home server

--- scripts/update_cloudflare_record.sh
+++ scripts/update_cloudflare_record.sh
@@ -2,7 +2,16 @@
 
 # Requires - cfcli https://blog.droidzone.in/2017/04/20/using-cloudflare-command-line-interface/
 
+NAMESERVER='bonnie.ns.cloudflare.com'
+RECORD='jabawoki-ext.maishsk.com'
+CURRENT_IP=`dig +short $RECORD @$NAMESERVER`
 EXTERNAL_IP=`curl -s ifconfig.co`
-RECORD='coder'
 
-cfcli -t A edit $RECORD $EXTERNAL_IP
+if [[ $EXTERNAL_IP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  if [[ $EXTERNAL_IP != $CURRENT_IP ]]; then
+    /usr/local/bin/cfcli -t A edit $RECORD $EXTERNAL_IP
+  fi  
+else
+  echo "Not a valid IP - Exiting"
+  exit 1
+fi
