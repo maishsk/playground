3b4afa0220a5ebbd80b04956ada148f80c81ee09
Maish Saidel-Keesing <maishsk@gmail.com>
2020-03-31 09:24:39 +0300

    made fixes added timestamp for logging

--- scripts/update_cloudflare_record.sh
+++ scripts/update_cloudflare_record.sh
@@ -2,6 +2,8 @@
 
 # Requires - cfcli https://blog.droidzone.in/2017/04/20/using-cloudflare-command-line-interface/
 
+. $HOME/.bashrc
+
 NAMESERVER='bonnie.ns.cloudflare.com'
 RECORD='jabawoki-ext.maishsk.com'
 CURRENT_IP=`dig +short $RECORD @$NAMESERVER`
@@ -9,9 +11,12 @@ EXTERNAL_IP=`curl -s ifconfig.co`
 
 if [[ $EXTERNAL_IP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   if [[ $EXTERNAL_IP != $CURRENT_IP ]]; then
+    printf "$(date)\n\n"
     /usr/local/bin/cfcli -t A edit $RECORD $EXTERNAL_IP
+  else
+    echo "$(date): IP has not changed"
   fi  
 else
-  echo "Not a valid IP - Exiting"
+  echo "$(date): Not a valid IP - Exiting"
   exit 1
 fi
