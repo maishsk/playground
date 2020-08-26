cf684ca7b1f2a8274b9fc862aa9780bdad0087d3
Maish Saidel-Keesing <maishsk@gmail.com>
2020-08-26 10:05:34 +0300

    added skeleton for future script use

--- scripts/update_keypair.sh
+++ scripts/update_keypair.sh
@@ -12,3 +12,10 @@ for region in $REGIONS
     aws ec2 import-key-pair --key-name $1 \
     --public-key-material fileb://~/.ssh/$1.pub --region $region
   done
+
+
+# Get personal token password for Github from parameter store"
+# GITHUB_TOKEN=$(aws ssm get-parameters --names /github/maishsk/personal_token --with-decryption --query "Parameters[0].Value" | tr -d '"')
+
+#curl -u "maishsk:$GITHUB_TOKEN" \
+#--data '{"title":"test-key","key":"ssh-rsa AAAA..."}' https://api.github.com/user/keys
